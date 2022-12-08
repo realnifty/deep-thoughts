da9007e0625b49ed08d1afb820bf2afdfab5cec1
realnifty <rogelioalbertopedraza@gmail.com>
2022-12-08 02:31:02 -0600

    server-client communication established

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 // import ApolloServer
+
+const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
 
@@ -19,6 +21,15 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
