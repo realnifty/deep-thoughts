83aedaca2e971b85337932cc9e59e510d29e74cb
realnifty <rogelioalbertopedraza@gmail.com>
2022-12-13 23:37:33 -0600

    signup/login authentication features, conditional component rendering

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 import Home from './pages/Home';
 import Header from './components/Header';
@@ -15,9 +16,19 @@ const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
+const authLink = setContext((_, { headers }) => {
+	const token = localStorage.getItem('id_token');
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : '',
+		},
+	};
+});
+
 const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache(),
+	link: authLink.concat(httpLink),
+	cache: new InMemoryCache(),
 });
 
 function App() {
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,13 @@
 import React, { useState } from 'react';
 
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
+
 const Login = (props) => {
   const [formState, setFormState] = useState({ email: '', password: '' });
+
+  const [login, { error }] = useMutation(LOGIN_USER);
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -15,49 +21,55 @@ const Login = (props) => {
 
   // submit form
   const handleFormSubmit = async (event) => {
-    event.preventDefault();
+		event.preventDefault();
 
-    // clear form values
-    setFormState({
-      email: '',
-      password: '',
-    });
-  };
+		try {
+			const { data } = await login({
+				variables: { ...formState },
+			});
+
+      Auth.login(data.login.token);
+			console.log(data);
+		} catch (e) {
+			console.error(e);
+		}
+	};
 
   return (
-    <main className='flex-row justify-center mb-4'>
-      <div className='col-12 col-md-6'>
-        <div className='card'>
-          <h4 className='card-header'>Login</h4>
-          <div className='card-body'>
-            <form onSubmit={handleFormSubmit}>
-              <input
-                className='form-input'
-                placeholder='Your email'
-                name='email'
-                type='email'
-                id='email'
-                value={formState.email}
-                onChange={handleChange}
-              />
-              <input
-                className='form-input'
-                placeholder='******'
-                name='password'
-                type='password'
-                id='password'
-                value={formState.password}
-                onChange={handleChange}
-              />
-              <button className='btn d-block w-100' type='submit'>
-                Submit
-              </button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </main>
-  );
+		<main className="flex-row justify-center mb-4">
+			<div className="col-12 col-md-6">
+				<div className="card">
+					<h4 className="card-header">Login</h4>
+					<div className="card-body">
+						<form onSubmit={handleFormSubmit}>
+							<input
+								className="form-input"
+								placeholder="Your email"
+								name="email"
+								type="email"
+								id="email"
+								value={formState.email}
+								onChange={handleChange}
+							/>
+							<input
+								className="form-input"
+								placeholder="******"
+								name="password"
+								type="password"
+								id="password"
+								value={formState.password}
+								onChange={handleChange}
+							/>
+							<button className="btn d-block w-100" type="submit">
+								Submit
+							</button>
+						</form>
+						{error && <div>Login failed</div>}
+					</div>
+				</div>
+			</div>
+		</main>
+	);
 };
 
 export default Login;
